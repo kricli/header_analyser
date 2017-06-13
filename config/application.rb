9f71af517c2c8b2b495468d04b05b845c92b545c
Richard Li <xpp@me.com>
2017-06-13 20:17:44 +0100

    feat(login): fix dotenv; ready to deploy

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module IMPLEMENTATION
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 5.1
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
